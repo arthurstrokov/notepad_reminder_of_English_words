d265b5e7f90fda7ab93cef0defdffa71713fdca5
arthurstrokov <arthurstrokov@gmail.com>
2019-11-06 19:18:45 +0300

    up design

--- ui/mydesign.py
+++ ui/mydesign.py
@@ -13,17 +13,17 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(500, 183)
+        MainWindow.resize(651, 189)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(400, 20, 90, 25))
+        self.pushButton.setGeometry(QtCore.QRect(540, 20, 90, 25))
         self.pushButton.setObjectName("pushButton")
         self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(20, 10, 371, 41))
+        self.label.setGeometry(QtCore.QRect(20, 10, 481, 41))
         self.label.setObjectName("label")
         self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_2.setGeometry(QtCore.QRect(400, 70, 90, 25))
+        self.pushButton_2.setGeometry(QtCore.QRect(540, 70, 90, 25))
         self.pushButton_2.setObjectName("pushButton_2")
         self.label_2 = QtWidgets.QLabel(self.centralwidget)
         self.label_2.setGeometry(QtCore.QRect(20, 60, 371, 41))
@@ -35,14 +35,14 @@ class Ui_MainWindow(object):
         self.lineEdit.setGeometry(QtCore.QRect(20, 120, 171, 20))
         self.lineEdit.setObjectName("lineEdit")
         self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_3.setGeometry(QtCore.QRect(200, 120, 75, 21))
+        self.pushButton_3.setGeometry(QtCore.QRect(200, 120, 91, 21))
         self.pushButton_3.setObjectName("pushButton_3")
         self.label_4 = QtWidgets.QLabel(self.centralwidget)
-        self.label_4.setGeometry(QtCore.QRect(290, 120, 201, 21))
+        self.label_4.setGeometry(QtCore.QRect(300, 120, 331, 21))
         self.label_4.setObjectName("label_4")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 500, 21))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 651, 22))
         self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
@@ -56,12 +56,14 @@ class Ui_MainWindow(object):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.pushButton.setText(_translate("MainWindow", "PushButton"))
-        self.label.setText(_translate("MainWindow", "Two thousand most frequently used words of the english language"))
+        self.label.setText(_translate(
+            "MainWindow", " Two thousand most frequently used words of the english language"))
         self.pushButton_2.setText(_translate("MainWindow", "PushButton"))
         self.label_2.setText(_translate("MainWindow", "unknown words yet"))
-        self.label_3.setText(_translate("MainWindow", "enter new unknown word:"))
-        self.pushButton_3.setText(_translate("MainWindow", "translate ->"))
-        self.label_4.setText(_translate("MainWindow", "translated"))
+        self.label_3.setText(_translate(
+            "MainWindow", "enter new unknown word:"))
+        self.pushButton_3.setText(_translate("MainWindow", " translate ->"))
+        self.label_4.setText(_translate("MainWindow", " translated"))
 
 
 if __name__ == "__main__":
